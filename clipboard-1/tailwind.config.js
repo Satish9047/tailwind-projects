d2b58b75f0c1cf8592e2bbdde299de4b69fbd6be
Satish9047 <satishprajapati930@gmail.com>
2024-04-18 08:39:01 +0545

    initilized setup the configure for the clipboard project

---
+++ clipboard-1/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "977px",
+      xl: "1440px",
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Bai  Jamjuree", "sans-serif"],
+      },
+      colors: {
+        strongCyan: "hsl(171, 66%, 44%)",
+        lightBlue: "hsl(233, 100%, 69%)",
+        darkGrayishBlue: "hsl(210, 10%, 33%)",
+        grayishBlue: "hsl(201, 11%, 66%)",
+      },
+    },
+  },
+  plugins: [],
+};
